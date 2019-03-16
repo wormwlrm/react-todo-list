fb84b9a806bd61a40ce33486ed8c1b66ff7a0d9c
wormwlrm <wormwlrm@naver.com>
2019-03-16 22:12:37 +0900

    Updates

---
+++ precache-manifest.5cb2b99d689f2f129edb0feafbbf8206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  },
+  {
+    "revision": "1543976cf87c3e6ca714",
+    "url": "/react-todo-list/static/js/main.af508e5e.chunk.js"
+  },
+  {
+    "revision": "02c1bacc98a896e00bc9",
+    "url": "/react-todo-list/static/js/2.7091c76f.chunk.js"
+  },
+  {
+    "revision": "1543976cf87c3e6ca714",
+    "url": "/react-todo-list/static/css/main.e1d0e84f.chunk.css"
+  },
+  {
+    "revision": "a9445e73e325fc4838573742b28ea575",
+    "url": "/react-todo-list/index.html"
+  }
+];
